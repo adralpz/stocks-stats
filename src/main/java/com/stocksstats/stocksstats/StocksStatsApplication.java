73aa2ca55af45f16c7e5afbba635f72abc8c6c51
negativems <mohamedbenhamzaa@gmail.com>
2024-06-15 17:12:33 +0200

    :tada: initial commit

---
+++ src/main/java/com/stocksstats/stocksstats/StocksStatsApplication.java
@@ -0,0 +1,13 @@
+package com.stocksstats.stocksstats;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StocksStatsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(StocksStatsApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/stocksstats/stocksstats/StocksStatsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.stocksstats.stocksstats;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StocksStatsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
