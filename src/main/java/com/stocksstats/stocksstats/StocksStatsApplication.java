e34525953fe7a5d1e36e791e8743f74fee5cee30
negativems <mohamedbenhamzaa@gmail.com>
2024-06-15 19:06:16 +0200

    Add stock symbol mentions tracking functionality

--- src/main/java/com/stocksstats/stocksstats/StocksStatsApplication.java
+++ src/main/java/com/stocksstats/stocksstats/StocksStatsApplication.java
@@ -3,8 +3,7 @@ package com.stocksstats.stocksstats;
 import masecla.reddit4j.client.Reddit4J;
 import masecla.reddit4j.client.UserAgentBuilder;
 import masecla.reddit4j.exceptions.AuthenticationException;
-import masecla.reddit4j.objects.RedditPost;
-import masecla.reddit4j.objects.RedditUser;
+import masecla.reddit4j.objects.RedditComment;
 import masecla.reddit4j.objects.Sorting;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -12,7 +11,14 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import java.io.FileInputStream;
 import java.io.IOException;
 import java.io.InputStream;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.List;
 import java.util.Properties;
+import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.TimeUnit;
 
 @SpringBootApplication
 public class StocksStatsApplication {
@@ -30,10 +36,72 @@ public class StocksStatsApplication {
                 .setClientSecret(props.getProperty("secret-key"))
                 .setUserAgent(new UserAgentBuilder().appname("stocks-stats").author("putotonto").version("1.0"));
 
+        // read file 'symbols.txt' and get list of symbols
+        List<String> symbols = Files.readAllLines(Paths.get("src/main/resources/stocks.txt"));
+
         client.userlessConnect();
 
-        client.getSubredditPosts("wallstreetbets", Sorting.TOP).submit().forEach(System.out::println);
+        // ConcurrentHashMap to handle concurrent modifications
+        var stockMentions = new ConcurrentHashMap<String, Integer>();
 
+        String subreddit = "wallstreetbets";
+
+//        symbols.forEach(symbol -> {
+//            executor.execute(() -> {
+//                res.stream()
+//                        .forEach(post -> {
+//                            try {
+//                                List<RedditComment> comments = client.getCommentsForPost(subreddit, post.getId()).submit();
+//                                for (RedditComment comment : comments) {
+//                                    if (comment.getBody().contains(symbol)) {
+//                                        synchronized (stockMentions) {
+//                                            stockMentions.put(symbol, stockMentions.getOrDefault(symbol, 0) + 1);
+//                                        }
+//                                    }
+//                                }
+//                            } catch (Exception ignored) {
+//                            }
+//                        });
+//            });
+//        });
+//
+
+
+        ExecutorService executor = Executors.newFixedThreadPool(20);
+
+        var res = client.getSubredditPosts(subreddit, Sorting.NEW).submit();
+        res.stream().forEach(post -> {
+            executor.execute(() -> {
+                try {
+                    List<RedditComment> comments = client.getCommentsForPost(subreddit, post.getId()).submit();
+                    for (RedditComment comment : comments) {
+                        String body = comment.getBody();
+
+                        symbols.forEach(symbol -> {
+                            if (body != null && body.contains(symbol)) {
+                                stockMentions.merge(symbol, 1, Integer::sum);
+                                System.out.println(comment.getAuthor() + "\n-----------------------------\n" + body);
+                                System.out.println("-----------------------------\n");
+                            }
+                        });
+                    }
+                    // sleep for 1 second to avoid rate limit
+                    Thread.sleep(1000);
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+            });
+        });
+
+        executor.shutdown();
+        try {
+            if (!executor.awaitTermination(60, TimeUnit.SECONDS)) {
+                executor.shutdownNow();
+            }
+        } catch (InterruptedException e) {
+            executor.shutdownNow();
+        }
+
+        stockMentions.forEach((k, v) -> System.out.println(k + " " + v));
     }
-
 }
