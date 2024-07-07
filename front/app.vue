84ae8f951a3520f3aeb742543cc40ebdf2f7f86c
negativems <negativems1@gmail.com>
2024-07-07 12:01:06 +0200

    chore: Refactor stock table pagination and null handling

--- front/app.vue
+++ front/app.vue
@@ -18,14 +18,15 @@ const chartOptions = ref({
 const { data, error } = await useFetch('http://192.168.1.147:8080/api/stocks');
 if (error) console.error(error.value);
 
-const stocks = ref<string[]>(data.value as any); 
+const stocks = ref<string[] | null>(data.value); 
 
 const page = ref(1);
 const pageCount = ref(10);
 const rows = computed(() => {
+  if (!stocks.value) return [];
   const startIndex = (page.value - 1) * pageCount.value;
   const endIndex = startIndex + pageCount.value;
-  return stocks.value.slice(startIndex, endIndex).map((stock, index) => ({
+  return stocks.value?.slice(startIndex, endIndex).map((stock, index) => ({
     id: index,
     name: stock,
   }));
@@ -37,7 +38,7 @@ const rows = computed(() => {
     <UCard class="mt-10">
       <template #header>
         <div class="flex justify-between">
-          <h1>Welcome to Nuxt UI Starter</h1>
+          <h1>Stock stats chart</h1>
           <ColorScheme>
             <USelect v-model="$colorMode.preference" :options="['system', 'light', 'dark']" />
           </ColorScheme>
@@ -51,7 +52,7 @@ const rows = computed(() => {
         <UTable :rows="rows" />
 
         <div class="flex justify-end px-3 py-3.5 border-t border-gray-200 dark:border-gray-700">
-          <UPagination v-model="page" :page-count="pageCount" :total="stocks.length" />
+          <UPagination v-model="page" :page-count="pageCount" :total="stocks?.length || 0" />
         </div>
       </div>
     </UCard>
