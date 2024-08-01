d9b1e5d16bdaeae34db38ca76511cd798635ec54
adrianstrong <adrian7nothing@gmail.com>
2024-08-01 23:27:51 +0200

    unsued imports

--- src/main/java/com/stocksstats/stocksstats/config/Initializer.java
+++ src/main/java/com/stocksstats/stocksstats/config/Initializer.java
@@ -8,14 +8,8 @@ import masecla.reddit4j.client.UserAgentBuilder;
 import masecla.reddit4j.exceptions.AuthenticationException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.io.Resource;
-import org.springframework.web.cors.CorsConfiguration;
-import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
-import org.springframework.web.filter.CorsFilter;
-import org.springframework.web.servlet.config.annotation.CorsRegistry;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 import java.io.IOException;
 import java.util.LinkedHashMap;
