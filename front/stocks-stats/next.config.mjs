667bc79b4ce0a9e2bf49b3f4507458d4a02ea734
adrianstrong <adrian7nothing@gmail.com>
2024-08-02 14:32:16 +0200

    next config edited just to deploy prod

--- front/stocks-stats/next.config.mjs
+++ front/stocks-stats/next.config.mjs
@@ -1,8 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: 'export',
-  basePath: '/stocks-stats',
-  assetPrefix: '/stocks-stats/',
   images: {
     unoptimized: true,
   },
